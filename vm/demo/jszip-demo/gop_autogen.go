e9676b1c3c12c07870520301d34254aaf3023eb5
xushiwei <x@goplus.org>
2024-01-27 01:19:48 +0800

    autogen

--- vm/demo/jszip-demo/gop_autogen.go
+++ vm/demo/jszip-demo/gop_autogen.go
@@ -10,16 +10,16 @@ const _ = true
 type loadsb3 struct {
 	yap.App
 }
-//line vm/demo/loadsb3/loadsb3_yap.gox:3
+//line vm/demo/jszip-demo/loadsb3_yap.gox:3
 func (this *loadsb3) MainEntry() {
-//line vm/demo/loadsb3/loadsb3_yap.gox:3:1
+//line vm/demo/jszip-demo/loadsb3_yap.gox:3:1
 	this.Static__0("/dist", os.DirFS("../../dist"))
-//line vm/demo/loadsb3/loadsb3_yap.gox:4:1
+//line vm/demo/jszip-demo/loadsb3_yap.gox:4:1
 	this.Static__0("/")
-//line vm/demo/loadsb3/loadsb3_yap.gox:6:1
+//line vm/demo/jszip-demo/loadsb3_yap.gox:6:1
 	this.Run(":8888")
 }
 func main() {
-//line vm/demo/loadsb3/loadsb3_yap.gox:6:1
+//line vm/demo/jszip-demo/loadsb3_yap.gox:6:1
 	yap.Gopt_App_Main(new(loadsb3))
 }
